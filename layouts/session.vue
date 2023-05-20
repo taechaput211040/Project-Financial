3608785557f2c4894e731f829de32e7f2ed14261
Tanakorn Aksorn <taechaphat>
2023-05-20 16:46:34 +0700

    update

--- layouts/session.vue
+++ layouts/session.vue
@@ -7,10 +7,14 @@
 </template>
 
 <script>
+import dayjs from "dayjs";
 export default {
   fetch() {
     this.$store.commit("set_logout");
     localStorage.clear();
+
+    console.log(dayjs('16:30:01', 'HH:mm:ss').get('h'));
+  
   }
 };
 </script>
